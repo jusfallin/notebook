8d4edf60030df31adad715be93bf3610b84c00c1
jusfallin <deepanb0309@gmail.com>
2026-09-07 23:13:52 +0530

    style: load full mode screen redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import './first-page.css';
 import './romance-interactions.css';
 import './love-atmosphere.css';
 import './toolbar-guide.css';
+import './mode-home-redesign.css';
+import './notebook-overrides.css';
 import ConfirmProvider from './confirm-provider';
 
 export const metadata: Metadata = {
@@ -19,12 +21,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body>
-        <ConfirmProvider />
-        {children}
-      </body>
-    </html>
-  );
+  return <html lang="en"><body><ConfirmProvider />{children}</body></html>;
 }
